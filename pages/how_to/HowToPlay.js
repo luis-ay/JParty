cc0a903a4c5d6e158c9e60b5923bcb8f0b388b82
Shmubyte <timmujeci@gmail.com>
2023-05-30 10:04:40 -0700

    Added both svgs, and formatted them.

--- pages/how_to/HowToPlay.js
+++ pages/how_to/HowToPlay.js
@@ -1,6 +1,7 @@
 import { StyleSheet, Text, View, Pressable, ScrollView } from 'react-native'
 import React from 'react'
-/*import SvgComponent from "./svgComponent"*/
+import QuizSVG from "./QuizSVG"
+import HostSVG from "./HostSVG"
 
 
 const HowToPlay = ({navigation}) => {
@@ -14,30 +15,34 @@ const HowToPlay = ({navigation}) => {
        
         <Text style={styles.sectionTitle}>Note:</Text>
         <View style={styles.underline}></View>
-        <Text style={styles.sectionInfo}>This application works best if all participants are on the same wifi network or hotspot connection. </Text>
-       
-        <Text style={styles.sectionTitle}>Roles</Text>
-        <View style={styles.underline}></View>
-        <Text style={styles.sectionInfo}>One lucky player will take on the role of the host, while the remaining participants will become contestants. Either way, everyone’s trivia knowledge will be put to the test.</Text>      
+        <Text style={styles.sectionInfo}>This application works best if all of the participants are connected to the same Wi-Fi network or Hotspot connection.{'\n'}</Text>
         
-        <Text style={styles.sectionTitle}>Re!Buzz</Text>
+        <QuizSVG/>
+        
+        <Text style={styles.sectionTitle}>{'\n'}Roles</Text>
         <View style={styles.underline}></View>
-        <Text style={styles.sectionInfo}>In Re!Buzz mode, when a contestant buzzes in and provides an answer, the excitement doesn't end there. If the first player who buzzed in gives an incorrect response, the opportunity immediately shifts to the other contestants, giving them another shot at buzzing in and answering the question correctly.
-         This feature adds an extra layer of strategy and anticipation to the game. As the tension builds, contestants must stay alert and ready to seize the moment when their opponents stumble. Re!Buzz mode allows for a thrilling back-and-forth battle as participants compete to be the first to buzz in with the correct response and earn valuable points.
-         Whether you're waiting to pounce on a missed answer or hoping to recover from a near-miss, Re!Buzz mode keeps the energy high and the competition intense. It's the perfect option for those seeking a fast-paced, engaging Jeopardy experience that keeps everyone on their toes.
-         So, get ready to outwit, outbuzz, and outscore your opponents in Re!Buzz mode. Will you seize the opportunity to turn the tables and prove yourself as the ultimate Jeopardy champion? The challenge awaits!    </Text>
+        <Text style={styles.sectionInfo}>One player will take on the role of host, while the remaining participants will become contestants.{'\n'}{'\n'}
+        As the host you will have the responsibility of:{'\n'}{'\n'}   1. Ensuring all participants are connected and ready to play.{'\n'}{'\n'}   2. Starting the game and maintaining its flow.{'\n'}{'\n'}   3. Awarding or deducting points for correct and incorrect answers.{'\n'}{'\n'}   4. Dismissing questions if the answer has been revealed before players on their devices have answered.{'\n'}{'\n'}   5. Awarding Daily Doubles and overseeing wagering.{'\n'}{'\n'}   6. Leading Final Jeopardy, including presenting the category and clue.</Text>  
+    
+        <HostSVG/>
 
-        <Text style={styles.sectionTitle}>Quick!Shift</Text>
-        <View style={styles.underline}></View>
-        <Text style={styles.sectionInfo}>When a contestant buzzes in and provides an answer, the pressure is on! If the first player who buzzed in gives an incorrect response, the opportunity immediately shifts to the next contestant in line. This means that the game keeps moving quickly, giving each player a chance to showcase their knowledge and potentially steal the points!
-         With Quick Shift, the intensity builds as contestants eagerly wait for their opportunity to pounce on a missed answer and take their shot at securing points. It adds an element of strategy and timing to the game, as players must balance their desire to answer quickly with the risk of getting it wrong and passing the opportunity to the next participant.
-         This feature brings an extra layer of excitement and unpredictability to your Jeopardy experience, making every moment count. So, get ready to quicken the pace, test your knowledge, and seize the opportunity to shine in Quick Shift mode!</Text>
+        <Text style={styles.sectionInfo}>As a contestant your main responsibilities include:{'\n'}{'\n'}   1. Joining the host's game session.{'\n'}{'\n'}   2. Getting ready to compete. Be attentive and prepare to buzz in.{'\n'}{'\n'}   3. Buzz in promptly and provide your response within the allotted time.</Text>
 
-        <Text style={styles.sectionTitle}>Deductions Disabled!</Text>
+
+        <Text style={styles.sectionTitle}>RE!BUZZ</Text>
         <View style={styles.underline}></View>
-        <Text style={styles.sectionInfo}>We understand that different players may have varying preferences for the level of competition they seek. Therefore, we provide the option to disable deductions, allowing for a less competitive experience.
+        <Text style={styles.sectionInfo}>In RE!BUZZ mode, if the contestant who buzzes in gives an incorrect response, the opportunity to buzz in becomes immediately available to all of the other contestants.{'\n'}{'\n'}</Text>
+
+        <Text style={styles.sectionTitle}>QUICK!SHIFT</Text>
+        <View style={styles.underline}></View>
+        <Text style={styles.sectionInfo}>In QUICK!SHIFT mode, the contestants get to answer in the order that they buzzed in. If the contestant who buzzes in gives an incorrect response, the second contestant who buzzed in now has the opportunity to answer, up to however many contestants buzzed in.{'\n'}{'\n'}(This mode may prove to be the better option if you're trying to play alongside the show.)</Text>
+
+        <Text style={styles.sectionTitle}>DEDUCTIONS DISABLED!</Text>
+        <View style={styles.underline}></View>
+        <Text style={styles.sectionInfo}>We provide the option to disable deductions, allowing for a less competitive experience.
          By disabling deductions, players can enjoy the game without the worry of losing points for incorrect answers. This feature creates a more relaxed and casual atmosphere, emphasizing participation, learning, and fun rather than the competitive aspect of scoring.
-         Whether you're looking for a friendly trivia gathering or a low-pressure learning experience, disabling deductions offers flexibility and customization to match your desired style of play. It ensures that everyone can enjoy the game at their own pace and comfort level, fostering a more inclusive and enjoyable Jeopardy experience for all participants.</Text>
+         Whether you're looking for a friendly trivia gathering or a low-pressure learning experience, disabling deductions offers flexibility and customization to match your desired style of play. It ensures that everyone can enjoy the game at their own pace and comfort level, 
+         fostering a more inclusive and enjoyable Jeopardy experience for all participants.</Text>
 
         <Pressable onPress={() => navigation.navigate('Main')}>
           <Text style={{fontSize:36, color:'white', top:'50%'}}>Back</Text>
@@ -79,7 +84,7 @@ const styles = StyleSheet.create({
     marginLeft:'15%',
     marginRight:'5%',
     marginTop:'5%',
-    marginBottom:'5%',
+    marginBottom:'10%',
     color:'white',
   },
   underline: {
