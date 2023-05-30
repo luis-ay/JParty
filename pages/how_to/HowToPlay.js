8e24e26dcadac875430f5eeb8268f1114cb64731
Shmubyte <timmujeci@gmail.com>
2023-05-30 15:38:18 -0700

    Added free for all to settings (needs button functionality), and adjusted how to play text sizes/distances.

--- pages/how_to/HowToPlay.js
+++ pages/how_to/HowToPlay.js
@@ -11,7 +11,7 @@ const HowToPlay = ({navigation}) => {
 
         <Text style={styles.sectionTitle}>Overview</Text>
         <View style={styles.underline}></View>
-        <Text style={styles.sectionInfo}>Introducing J ! Party, the ultimate Jeopardy companion app that brings the excitement of the iconic game show right to your fingertips! With J ! Party, you can host your very own trivia showdown with up to __ friends or family members, as you battle it out to become the J ! Party champion.</Text>
+        <Text style={styles.sectionInfo}>Introducing J <Text style={styles.logoColor}>!</Text> Party, the ultimate Jeopardy companion app that brings the excitement of the iconic game show right to your fingertips! With J <Text style={styles.logoColor}>!</Text> Party, you can host your very own trivia showdown with up to __ friends or family members, as you battle it out to become the J <Text style={styles.logoColor}>!</Text> Party champion.</Text>
        
         <Text style={styles.sectionTitle}>Note:</Text>
         <View style={styles.underline}></View>
@@ -21,28 +21,28 @@ const HowToPlay = ({navigation}) => {
         
         <Text style={styles.sectionTitle}>{'\n'}Roles</Text>
         <View style={styles.underline}></View>
-        <Text style={styles.sectionInfo}>One player will take on the role of host, while the remaining participants will become contestants.{'\n'}{'\n'}
-        As the host you will have the responsibility of:{'\n'}{'\n'}   1. Ensuring all participants are connected and ready to play.{'\n'}{'\n'}   2. Starting the game and maintaining its flow.{'\n'}{'\n'}   3. Awarding or deducting points for correct and incorrect answers.{'\n'}{'\n'}   4. Dismissing questions if the answer has been revealed before players on their devices have answered.{'\n'}{'\n'}   5. Awarding Daily Doubles and overseeing wagering.{'\n'}{'\n'}   6. Leading Final Jeopardy, including presenting the category and clue.</Text>  
+        <Text style={styles.sectionInfo}>One player will take on the role of <Text style={styles.hostColor}>host</Text>, while the remaining participants will become contestants.{'\n'}{'\n'}
+        As the <Text style={styles.hostColor}>host</Text> you will have the responsibility of:{'\n'}{'\n'}   1. Ensuring all participants are{'\n'}{'\t'} connected and ready to play.{'\n'}{'\n'}   2. Starting the game and maintaining{'\n'}{'\t'}  its flow.{'\n'}{'\n'}   3. Awarding or deducting points for {'\n'}{'\t'}  correct and incorrect answers.{'\n'}{'\n'}   4. Dismissing questions if the answer{'\n'}{'\t'}  has been revealed before players{'\n'}{'\t'}  on their devices have answered.{'\n'}{'\n'}   5. Awarding Daily Doubles and {'\n'}{'\t'}  overseeing wagering.{'\n'}{'\n'}   6. Orienting Final J <Text style={styles.logoColor}>!</Text> Party.</Text>  
     
         <HostSVG/>
 
-        <Text style={styles.sectionInfo}>As a contestant your main responsibilities include:{'\n'}{'\n'}   1. Joining the host's game session.{'\n'}{'\n'}   2. Getting ready to compete. Be attentive and prepare to buzz in.{'\n'}{'\n'}   3. Buzz in promptly and provide your response within the allotted time.</Text>
+        <Text style={styles.sectionInfo}>As a <Text style={styles.logoColor}>contestant</Text> your main responsibilities include:{'\n'}{'\n'}   1. Joining the <Text style={styles.hostColor}>host</Text>'s game session.{'\n'}{'\n'}   2. Being ready to compete.{'\n'}{'\n'}   3. Buzzing in promptly, and providing{'\n'}{'\t'}  your response within the allotted {'\n'}{'\t'}  time.</Text>
 
-
-        <Text style={styles.sectionTitle}>RE!BUZZ</Text>
+        <Text style={styles.sectionTitle}>FREE FOR ALL<Text style={styles.logoColor}>!</Text></Text>
         <View style={styles.underline}></View>
-        <Text style={styles.sectionInfo}>In RE!BUZZ mode, if the contestant who buzzes in gives an incorrect response, the opportunity to buzz in becomes immediately available to all of the other contestants.{'\n'}{'\n'}</Text>
+        <Text style={styles.sectionInfo}>The standard and lively way to play J <Text style={styles.logoColor}>!</Text> Party at home! Everyone who buzzes in gets to answer. Contestants compete by shouting out their answers, while the host tallies their scores. </Text>
 
-        <Text style={styles.sectionTitle}>QUICK!SHIFT</Text>
+        <Text style={styles.sectionTitle}>RE<Text style={styles.logoColor}>!</Text>BUZZ</Text>
         <View style={styles.underline}></View>
-        <Text style={styles.sectionInfo}>In QUICK!SHIFT mode, the contestants get to answer in the order that they buzzed in. If the contestant who buzzes in gives an incorrect response, the second contestant who buzzed in now has the opportunity to answer, up to however many contestants buzzed in.{'\n'}{'\n'}(This mode may prove to be the better option if you're trying to play alongside the show.)</Text>
+        <Text style={styles.sectionInfo}>If the contestant that buzzed in gives an incorrect response, the opportunity to "re-buzz" becomes available to all other contestants.</Text>
 
-        <Text style={styles.sectionTitle}>DEDUCTIONS DISABLED!</Text>
+        <Text style={styles.sectionTitle}>QUICK<Text style={styles.logoColor}>!</Text>SHIFT</Text>
         <View style={styles.underline}></View>
-        <Text style={styles.sectionInfo}>We provide the option to disable deductions, allowing for a less competitive experience.
-         By disabling deductions, players can enjoy the game without the worry of losing points for incorrect answers. This feature creates a more relaxed and casual atmosphere, emphasizing participation, learning, and fun rather than the competitive aspect of scoring.
-         Whether you're looking for a friendly trivia gathering or a low-pressure learning experience, disabling deductions offers flexibility and customization to match your desired style of play. It ensures that everyone can enjoy the game at their own pace and comfort level, 
-         fostering a more inclusive and enjoyable Jeopardy experience for all participants.</Text>
+        <Text style={styles.sectionInfo}>The contestants get to answer in the order that they buzzed in. This process continues up until a correct answer is given or until the question is dismissed.</Text>
+
+        <Text style={styles.sectionTitle}>DEDUCTIONS DISABLED<Text style={styles.logoColor}>!</Text></Text>
+        <View style={styles.underline}></View>
+        <Text style={styles.sectionInfo}>By disabling deductions, players can enjoy the game without the worry of losing points for incorrect answers.</Text>
 
         <Pressable onPress={() => navigation.navigate('Main')}>
           <Text style={{fontSize:36, color:'white', top:'50%'}}>Back</Text>
@@ -63,7 +63,7 @@ const styles = StyleSheet.create({
   },
   scrollcontainer: {
     marginTop:100,
-    paddingBottom: 150 ///This is very important for scrolling to the bottom, adjust as needed
+    paddingBottom: 900 ///This is very important for scrolling to the bottom, adjust as needed
   },
   logo:{
     fontSize:60,
@@ -81,14 +81,14 @@ const styles = StyleSheet.create({
   sectionInfo:{
     fontSize:18,
     justifyContent: 'center',
-    marginLeft:'15%',
+    marginLeft:'10%',
     marginRight:'5%',
     marginTop:'5%',
-    marginBottom:'10%',
+    marginBottom:'11%',
     color:'white',
   },
   underline: {
-    width: '50%',
+    width: '85%',
     bottom:'0%',
     left: '3.5%',
     height: 1,
@@ -97,6 +97,10 @@ const styles = StyleSheet.create({
   },
   logoColor: {
     color: '#6A41FF',
+    textDecorationLine: 'none',
+  },
+  hostColor:{
+    color:'#FFD700',
     textDecorationLine: 'none',
   },
 })
